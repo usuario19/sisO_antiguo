163dfed6ce84cca26df1be243ee91ed98cbc2dfe
maryangela <angelamdp16@gmail.com>
2017-11-30 18:23:50 -0400

    anghi

--- database/migrations/2014_10_12_000000_create_usuario_table.php
+++ database/migrations/2014_10_12_000000_create_usuario_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateUsuarioTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,16 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('name');
+        Schema::create('usuarios', function (Blueprint $table) {
+            $table->increments('ci')->primary;
+            $table->string('nombre');
+            $table->string('apellidos');
+            $table->date('fecha_nac');
+            $table->binary('foto');
             $table->string('email')->unique();
             $table->string('password');
+            $table->integer('id club')->unsigned();
+            $table->foreign('id_club')->references('id')->on('clubs');
             $table->rememberToken();
             $table->timestamps();
         });
@@ -30,6 +35,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('usuarios');
     }
 }
